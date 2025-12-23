2960924d2fcf90531e1fa253444390d06dc9ecb6
Spandana <spandanaaithagoni@gmail.com>
2025-12-23 14:51:14 +0530

    Local Changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-function showMessage() {
-    alert("Welcome to Campus Event Management System!");
+function showDetails(eventName) {
+    alert("You selected: " + eventName);
 }
+
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("successMsg").innerText =
+        "Registration successful! Thank you for registering.";
+});
